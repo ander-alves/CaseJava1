dd50c836cc9b8d3c074828bcaf549e989415cd4b
Anderson Alves <alves_anderson@outlook.com>
2024-03-29 20:37:21 -0300

    Create projetc

---
+++ src/main/java/com/case1/ponte/PonteApplication.java
@@ -0,0 +1,13 @@
+package com.case1.ponte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PonteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/case1/ponte/PonteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.case1.ponte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PonteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
