9503af950fc50ec4c795aaf3dc0e4b3bd3c89ce9
Anderson Alves <alves_anderson@outlook.com>
2024-03-29 23:44:35 -0300

    Add Documentation Swagger

--- src/main/java/com/case1/ponte/PonteApplication.java
+++ src/main/java/com/case1/ponte/PonteApplication.java
@@ -1,9 +1,11 @@
 package com.case1.ponte;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @io.swagger.v3.oas.annotations.info.Info(title = "Ponte API", version = "1.1", description = "Documentation Ponte API v1.0"))
 public class PonteApplication {
 
 	public static void main(String[] args) {
--- src/main/resources/db/migration/V1__Create_Produto_Table.sql
+++ src/main/resources/db/migration/V1__Create_Produto_Table.sql
@@ -1,6 +1,5 @@
 CREATE TABLE Produto (
                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
                          nome VARCHAR(255) NOT NULL,
-                         preco DECIMAL(10, 2) NOT NULL,
                          descricao VARCHAR(255)
 );
